4d27f32ffef4a0ab0b47da806f4c7e365f5120e6
Caroline <animals4all@gmail.com>
2015-06-21 19:27:56 -0400

    Getting the Player's Guess

--- guessing.py
+++ guessing.py
@@ -4,3 +4,9 @@ print "Welcome to the Number Guessing Game!"
 
 name = raw_input("Please enter your name: ")
 number = random.randint(1, 100)
+
+print "%s, try to guess the number I'm thinking of. It's between 1 and 100." % name
+print ""
+
+while True:
+	guess = int(raw_input("Your guess?: "))
