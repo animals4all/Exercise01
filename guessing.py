beab8329a940a73ec09053e2d06a281b6dc0452a
Caroline <animals4all@gmail.com>
2015-06-21 20:44:13 -0400

    Extra Credit

--- guessing.py
+++ guessing.py
@@ -4,20 +4,47 @@ print "Welcome to the Number Guessing Game!"
 
 name = raw_input("Please enter your name: ")
 number = random.randint(1, 100)
+highScore = 9999999999
+turnNumber = 0
 
 print "%s, try to guess the number I'm thinking of. It's between 1 and 100." % name
 print ""
 
 while True:
-	guess = int(raw_input("Your guess?: "))
-	if guess < number:
+	try:
+		guess = int(raw_input("Your guess?: "))
+		turnNumber += 1
+		if guess > 100 or guess < 1:
+			print ""
+			print "Your guess must be a number between 1 and 100."
+			print ""
+		else:
+			if guess < number:
+				print ""
+				print "Your guess is too low."
+				print ""
+			elif guess > number:
+				print ""
+				print "Your guess is too high."
+				print ""
+			else:
+				print ""
+				print "Congratulations, %s! You correctly guessed my number." % name, number
+				print ""
+				if turnNumber < highScore:
+					highScore = turnNumber
+					print "New record: %d" % highScore
+					print ""
+				turnNumber = 0
+				if input("Want to play again?: ").lower in ("y", "yes"):
+					name = raw_input("Please enter your name: ")
+					number = random.randint(1, 100)
+					print "%s, try to guess the number I'm thinking of. It's between 1 and 100." % name
+					print ""
+				else:
+					break
+
+	except ValueError:
 		print ""
-		print "Your guess is too low."
+		print "That's not a number!"
 		print ""
-	elif guess > number:
-		print ""
-		print "Your guess is too high."
-		print ""
-	else:
-		print "Congratulations, %s! You correctly guessed my number. It was %d." % (name, number)
-		break
