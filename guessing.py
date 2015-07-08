2f2e15390d7933cc4bb62d4fcae46644a3f675e2
Caroline <animals4all@gmail.com>
2015-07-08 11:43:11 -0400

    Final

--- guessing.py
+++ guessing.py
@@ -13,13 +13,13 @@ print ""
 while True:
 	try:
 		guess = int(raw_input("Your guess?: "))
-		turnNumber += 1
-		if guess > 100 or guess < 1:
+		if guess < 1 or guess > 100:
 			print ""
 			print "Your guess must be a number between 1 and 100."
 			print ""
 
 		else:
+			turnNumber += 1
 			if guess < number:
 				print ""
 				print "Your guess is too low."
@@ -30,7 +30,7 @@ while True:
 				print ""
 			else:
 				print ""
-				print "Congratulations, %s! You correctly guessed my number." % name, number
+				print "Congratulations, %s! You correctly guessed my number." % name
 				print ""
 				if turnNumber < highScore:
 					highScore = turnNumber
